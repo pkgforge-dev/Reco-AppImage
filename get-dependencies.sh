939b52982a37f71a0dff5b3e02d952190c88db15
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-01-21 10:35:28 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,7 +10,7 @@ get-debloated-pkgs --add-common --prefer-nano
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm vala
+pacman -Syu --noconfirm vala pipewire-audio
 make-aur-package live-chart
 make-aur-package ryokucha
 make-aur-package reco
