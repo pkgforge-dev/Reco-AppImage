a37e2069b578339a2dd4d9bf0bef589707962116
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-01-21 13:57:27 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -4,13 +4,14 @@ set -eu
 
 ARCH=$(uname -m)
 
+pacman -Syu --noconfirm vala pipewire-audio gst-libav gst-plugins-good gst-plugins-base
+
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm vala pipewire-audio gst-libav gst-plugins-good gst-plugins-base
 make-aur-package live-chart
 make-aur-package ryokucha
 make-aur-package reco
