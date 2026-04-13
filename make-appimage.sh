7e53f12ce65ec4513b92ba615576f7c44d006898
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-04-13 10:32:03 +0200

    Replace self-updater.bg.hook with self-updater.hook

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q reco | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/ryonakano/reco/refs/heads/main/data/icons/128/com.github.ryonakano.reco.svg
 export DESKTOP=/usr/share/applications/com.github.ryonakano.reco.desktop
